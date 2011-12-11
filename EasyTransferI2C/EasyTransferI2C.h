a93b1e3553f4d6e2d543acef4a4e1f24244fe86d
Mathieu Alorent <stairs@kumy.net>
2011-12-11 23:36:29 +0100

    Forked EasyTransfer to EasyTransferI2C
    This library use exactly the same protocol as EasyTransfer,
    but communicate via I2C instead of Uarts.

---
+++ EasyTransferI2C/EasyTransferI2C.h
@@ -0,0 +1,65 @@
+/******************************************************************
+*  EasyTransferI2C Arduino Library 
+*		details and example sketch: 
+*			http://www.billporter.info/easytransfer-arduino-library/
+*
+*		Brought to you by:
+*              Mathieu Alorent
+*               Forked from
+*              Bill Porter
+*              www.billporter.info
+*
+*		See Readme for other info and version history
+*	
+*  
+*This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+<http://www.gnu.org/licenses/>
+*
+*This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
+*To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or
+*send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+******************************************************************/
+#ifndef EasyTransferI2C_h
+#define EasyTransferI2C_h
+
+
+//make it a little prettier on the front end. 
+#define details(name) (byte*)&name,sizeof(name)
+
+//Not neccessary, but just in case. 
+#if ARDUINO > 22
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include "HardwareSerial.h"
+//#include <NewSoftSerial.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <avr/io.h>
+#include <Wire.h>
+
+class EasyTransferI2C {
+public:
+void begin(uint8_t *, uint8_t, TwoWire *theSerial);
+void sendData(uint8_t address);
+boolean receiveData();
+private:
+TwoWire *_serial;
+//NewSoftSerial *_serial;
+uint8_t * address;  //address of struct
+uint8_t size;       //size of struct
+uint8_t rx_len;		//RX packet length according to the packet
+uint8_t rx_array[255]; //RX packet parsing buffer
+uint8_t rx_array_inx;  //index for RX parsing buffer
+uint8_t calc_CS;	   //calculated Chacksum
+};
+
+
+
+#endif
---
+++ EasyTransferI2C/Examples/EasyTransfer_RX_Example/EasyTransfer_RX_Example.pde
@@ -0,0 +1,45 @@
+#include <Wire.h>
+#include <EasyTransferI2C.h>
+
+//create object
+EasyTransferI2C ET; 
+
+struct RECEIVE_DATA_STRUCTURE{
+  //put your variable definitions here for the data you want to receive
+  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
+  int blinks;
+  int pause;
+};
+
+//give a name to the group of data
+RECEIVE_DATA_STRUCTURE mydata;
+
+//define slave i2c address
+#define I2C_SLAVE_ADDRESS 9
+
+void setup(){
+  Wire.begin(I2C_SLAVE_ADDRESS);
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
+  ET.begin(details(mydata), &Wire);
+  //define handler function on receiving data
+  Wire.onReceive(receive);
+  
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop() {}
+
+void receive(int numBytes){
+  //check and see if a data packet has come in. 
+  if(ET.receiveData()){
+    //this is how you access the variables. [name of the group].[variable name]
+    //since we have data, we will blink it out. 
+    for(int i = mydata.blinks; i>0; i--){
+      digitalWrite(13, HIGH);
+      delay(mydata.pause * 100);
+      digitalWrite(13, LOW);
+      delay(mydata.pause * 100);
+    }
+  }
+}
